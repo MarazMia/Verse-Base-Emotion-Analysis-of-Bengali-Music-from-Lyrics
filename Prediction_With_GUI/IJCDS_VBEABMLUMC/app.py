f640e4a36536ca83186f8862a490733e084a6b6d
Maraz Mia <43583025+MarazMia@users.noreply.github.com>
2023-10-14 21:16:53 +0600

    Add files via upload

---
+++ Prediction_With_GUI/IJCDS_VBEABMLUMC/app.py
@@ -0,0 +1,63 @@
+#import libraries
+import numpy as np
+from flask import Flask, render_template,request
+import joblib
+#Initialize the flask App
+app = Flask(__name__)
+model1 = joblib.load('ML_Models/sgd2class_model.pickle')
+model2 = joblib.load('ML_Models/sgd3class_model.pickle')
+# ans = model.predict(["I love you"])[0]
+# print(ans)
+#for detecting verses
+def Verse_from_Bengali_Song(song: str)-> list:
+    all_verse = []
+    all_verse = song.split('\r\n\r\n')
+    if len(all_verse)==1:
+        all_verse = song.split('॥\r\n')
+    if len(all_verse)==1:
+        all_verse = song.split('।।\r\n')
+    for i in range(len(all_verse)):
+        all_verse[i] = " ".join(all_verse[i].split('\r\n')).strip()
+    return all_verse
+
+
+#To use the predict button in our web-app
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
+@app.route('/predict',methods=['POST'])
+def predict():
+    #For rendering results on HTML GUI
+    int_features = request.form.get('song_input')
+    # print(int_features.split('।।\r\n'))
+    # print(Verse_from_Bengali_Song(int_features))
+    # print(len(Verse_from_Bengali_Song(int_features)))
+
+    verses = Verse_from_Bengali_Song(int_features)
+    original_verses = ''
+    for i in range(len(verses)):
+        original_verses += "Verse "+str(i+1)+" "+verses[i]+'\n\n\n'
+    # print(verses)
+
+    output = ""
+    for i in range(len(verses)):  
+        prediction1 = model1.predict([verses[i]])
+        if prediction1==1:
+            output += "verse " + str(i+1) + " emotion : " + "Positively "
+        else:
+            output += "verse " + str(i+1) + " emotion : " + "Negatively "
+
+        prediction2 = model2.predict([verses[i]])
+        if prediction2==1:
+            output += "Love.\n"
+        elif prediction2==2:
+            output += "Sad.\n"
+        else:
+            output += "Life Oriented.\n"  
+
+    return render_template('index.html', prediction_text='{}'.format(output), original_text = original_verses)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
